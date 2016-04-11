d161e9ba50e11d8476f5445a6e65288370d649aa
jaken herman <jakenherman7@gmail.com>
2016-04-11 07:29:37 -0500

    initial commit

--- cookbook/main.swift
+++ cookbook/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    return NSString(data: inputData, encoding:NSUTF8StringEncoding) as! String
+} //thanks to Chalkers on Stack Overflow for this function
+var user_input = input()
+
+var command_array = user_input.componentsSeparatedByString(" ")
+
+if(command_array[1] == "javascript"){
+    print("Javascript")
+} else { print("Error: Tech stack not found") }
+
+
 
